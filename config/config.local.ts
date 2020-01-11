fed5b87af9513cd6ad68a6d07330ad4b16808409
zhangwenshun <vendy9229@163.com>
2020-01-11 21:58:13 +0800

    modify

--- config/config.local.ts
+++ config/config.local.ts
@@ -2,21 +2,6 @@ import { EggAppConfig, PowerPartial } from 'egg';
 
 export default () => {
   const config: PowerPartial<EggAppConfig> = {};
-  config.cors = {
-    // {string|Function} origin: '*',
-    // {string|Array} allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH'
-    origin: '*',
-    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH'
-  };
 
-  config.io = {
-    init: {}, // passed to engine.io
-    namespace: {
-      '/': {
-        connectionMiddleware: [ 'connection' ],
-        packetMiddleware: [],
-      }
-    },
-  };
   return config;
 };
