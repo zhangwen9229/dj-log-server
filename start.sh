ef2447fa45acafc4be0d10a5daf01c243a147d2c
zhangwenshun <zhangwenshun@imdada.cn>
2021-02-04 11:37:02 +0800

    增加start.sh

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+###
+ # @Date: 2021-02-04 11:35:32
+ # @LastEditors: zhangwenshun
+ # @LastEditTime: 2021-02-04 11:36:35
+ # @title: start
+ # @created by: zhangwenshun
+### 
+
+cd /home/venshun/web_workspace/other/dj-log-server
+
+npm start
